3cc4ae6ae00c3917d98e4154da84b1a44e6ea9c9
Your Name <guptamadhav1729@gmail.com>
2024-03-31 18:22:29 +0530

    computer position updated

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -58,7 +58,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [27, 3, 5], fov: 25 }}
+      camera={{ position: [40, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
